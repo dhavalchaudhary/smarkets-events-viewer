7e8fb4103ed94c16b255495f237092ebd73dccfb
Dhaval Chaudhary <dhavalchaudhary96@gmail.com>
2022-03-10 15:07:29 +0530

    added react router

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,3 +1,11 @@
 import React from 'react'
+import { BrowserRouter, Route } from 'react-router-dom';
 
-export const App = () => <h1>Hello World</h1> 
+export const App = () => {
+    return (
+        <BrowserRouter >
+            <Route exact path="/" render={() => <h1>Popular Events</h1>} />
+            <Route path="/event/:id" render={(props) => <h1>{JSON.stringify(props)}</h1>} />
+        </BrowserRouter>
+    )
+}
